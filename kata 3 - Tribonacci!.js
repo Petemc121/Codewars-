52a77f347ca077665bf18e07a1775cb35f786d04
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-17 12:09:14 +0900

    Update and rename kata 3 to kata 3 - Tribonacci!.js

--- kata 3 - Tribonacci!.js
+++ kata 3 - Tribonacci!.js
@@ -1,3 +1,6 @@
+//TASK: create a fibonacci function that given a signature array/list, returns the first n elements - 
+signature included of the so seeded sequence.
+
 function tribonacci(signature,n){
   
  
