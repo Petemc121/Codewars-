1424dcd6608f7ab5a61ba2a69df7b4475ddde334
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-17 12:19:37 +0900

    Update and rename kata 10 to kata 10 - Extract the domain name from a URL.js

--- kata 10 - Extract the domain name from a URL.js
+++ kata 10 - Extract the domain name from a URL.js
@@ -1,3 +1,5 @@
+//TASK Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
 function domainName(url){
 start1 = url.startsWith("h");
   start2 = url.startsWith("w");
