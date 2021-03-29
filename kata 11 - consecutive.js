237a8b40d6699366c36ad371e5057df5046c07e7
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-29 12:33:29 +0900

    Update and rename kata 10 - consecutive.js to kata 11 - consecutive.js

--- kata 11 - consecutive.js
+++ kata 11 - consecutive.js
@@ -1,17 +1,25 @@
 //A format for expressing an ordered list of integers is to use a comma separated list of either individual integers or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17" Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
-
 function solution(list){
   
 
-  
+  let newArray = []
  for(let i = 0;i<list.length;i++) {
-   n=1;
-  while(list[i+n] === list[i]+n) {
-    n++
-    list[i] == list[i] + "-" + (list[i] + n)
-    list.splice(i+n,1);
+   
+   if (list[i] === list[i+1]-1 && list[i] != list[i-1]+1)
+     {
+       let j = i + 1;
+       let start = i
+       while (list[j] === list[j-1]+1) {
+         j++
+         
+       }
+       
+     } else {
+       newArray.push(list[i]);
+     }
+ 
   }
-}
   
-  console.log(list);
+  console.log(newArray);
+  
   }
