bb35ffec7a0e81009121b0a123efafb427b49620
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-04-09 14:53:14 +0900

    Update kata 11 - consecutive.js
    After many convoluted and overcomplicated diversions from potential solutions to this problem, I finally figured it out! I simply pushed each array element onto a new array if they met certain criteria provided by the if statements

--- kata 11 - consecutive.js
+++ kata 11 - consecutive.js
@@ -1,77 +1,38 @@
 function solution(list){
   
 let markedArray = [];
-let finalArray = [];
-  const initList = list;
-  
-  console.log(initList);
-
+let start;
   
  for(let i = 0;i<list.length;i++) {
    
-   
-   if (list[i] === list[i+1]-1 && list[i] != list[i-1]+1)
+   if ((list[i] === list[i+1]-1) && (list[i] !== list[i-1]+1))
      {
-      
-       let first= i;
-       let second = i + 1;
-       let diff = list[first]-list[second];
+       start = list[i]
        
-      
-       
-       while (diff === 1 ){
-         second++
-         first++
-         list.splice(first,1); 
+     } else
          
-       }
+   if ((list[i] === list[i+1]-1) && (list[i] === list[i-1]+1)) {
        
-       let end = first
-       list[i] = list[i] + "-" + list[second];
-       markedArray.push(list[i]);
-       
+          
+      } else
+   
+   if ((list[i] !== list[i+1]-1) && (list[i] == list[i-1]+1)) {
+     
+      if ((list[i] - start) > 1) {
+         markedArray.push(start + "-" + list[i]);
      } else {
-        markedArray.push(list[i]);
+        markedArray.push(start);
+       markedArray.push(list[i]);
      }
-   
- 
- 
+     
+        }else {
+          markedArray.push(list[i]);
+        }
+  
+    
   }
-  
- console.log(markedArray);
-  
-   let start;
-  let end;
-    
-   for(let x = 0; x < markedArray.length; x++) {
-    
-     if(typeof markedArray[x] ==="string" && typeof markedArray[x-1] !=="string") 
-     {
-       
-     console.log('splice')
-       start = initList[x];
-        markedArray.splice(x,1)
- 
-       
-       
-      } else 
-     
-   if(typeof markedArray[x] === "string" && typeof markedArray[x+1] ==="string") 
-   {
-      
-     markedArray.splice(x,1)
-     console.log('splice')
-     x--
-     
-     } else if (typeof markedArray[x] ==="string" && typeof markedArray[x+1] !=="string") {
-       end = initList[x+2];
-       markedArray[x] = start + "-" + end;
-         
-     }
-     }
-  console.log(markedArray);
- 
-  
+  let stringify = markedArray.toString();
+ return stringify;
   }
   
 
