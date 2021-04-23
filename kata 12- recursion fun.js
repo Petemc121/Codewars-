355b99c074c479cd9067940097b6df744502727f
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-04-23 16:13:28 +0900

    Update kata 12- recursion fun.js
    fuck

--- kata 12- recursion fun.js
+++ kata 12- recursion fun.js
@@ -1,31 +1,8 @@
 //find the largest common divisor between two inputs
 
 function mygcd(x,y){
-  
-  if (x == 1 || y == 1) 
-    {
-      return 1;
-    }
-  
-   if (x > y) 
-   {
-     if(x % y == 0) {
-       return y;
-     } else {
-       return mygcd(x,y-1);
-     }
-   }
-else if (y > x) 
-  {
-    if (y % x == 0) 
-      {
-        return x;
-      } else {
-        return mygcd(x-1, y);
-      }
-  }
-  
-    }
+  return y == 0 ? x : mygcd(y, x % y)
+}
        
   
       
