bb4938d5a16b9e7209ebd23b69b06cce3a163efd
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-07-01 15:37:37 +0900

    Update kata 13 - biggestSubstring.js
    tried using different substring functions and taking into account string length differences but still doesn't work

--- kata 13 - biggestSubstring.js
+++ kata 13 - biggestSubstring.js
@@ -1,21 +1,48 @@
 function LCS(x, y) {
   let subsequence = "";
+  let tempY = y;
+  let tempX = x;
  
- 
+  if (y.length > x.length)
+    {
   for (let i = 0; i < x.length; i++)
     {
     
-      if (y.includes(x[i], i))
+     if (tempY.indexOf(x[i]) !== -1)
         {
-      
-            subsequence += x[i];
+          subsequence += x[i];
+        tempY = tempY.slice(0, tempY.indexOf(x[i])) + tempY.slice(tempY.indexOf(x[i]) + 1, tempY.length);
+            console.log(tempY)
+           console.log(tempX)
+          console.log(subsequence)
         }
-        
 
     }
+      }
+  else if (x.length > y.length)
+    {
+       for (let i = 0; i < y.length; i++)
+    {
+    
+     if (tempX.indexOf(y[i]) !== -1)
+        {
+          subsequence += y[i];
+         tempX = tempX.slice(0, tempX.indexOf(y[i])) + tempX.slice(tempX.indexOf(y[i]) + 1, tempX.length);
+          console.log(tempY)
+           console.log(tempX)
+          console.log(subsequence)
+        }
 
+    }
+    }
   
+
+
   return subsequence;
 }
+
+
+console.log(LCS('FJKASDFNK','SADFKJLML'))
+
 
 
