b5603f37879fd46cbad7417bba90a201c712bb5d
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-07-01 15:50:59 +0900

    Update kata 13 - biggestSubstring.js
    simplified the code

--- kata 13 - biggestSubstring.js
+++ kata 13 - biggestSubstring.js
@@ -2,9 +2,12 @@ function LCS(x, y) {
   let subsequence = "";
   let tempY = y;
   let tempX = x;
+  
+   console.log(tempY)
+           console.log(tempX)
+          console.log(subsequence)
  
-  if (y.length > x.length)
-    {
+ 
   for (let i = 0; i < x.length; i++)
     {
     
@@ -12,37 +15,23 @@ function LCS(x, y) {
         {
           subsequence += x[i];
         tempY = tempY.slice(0, tempY.indexOf(x[i])) + tempY.slice(tempY.indexOf(x[i]) + 1, tempY.length);
-            console.log(tempY)
-           console.log(tempX)
-          console.log(subsequence)
+           
         }
 
     }
-      }
-  else if (x.length > y.length)
-    {
-       for (let i = 0; i < y.length; i++)
-    {
-    
-     if (tempX.indexOf(y[i]) !== -1)
-        {
-          subsequence += y[i];
-         tempX = tempX.slice(0, tempX.indexOf(y[i])) + tempX.slice(tempX.indexOf(y[i]) + 1, tempX.length);
-          console.log(tempY)
-           console.log(tempX)
-          console.log(subsequence)
-        }
-
-    }
-    }
+     
   
-
+ console.log(tempY)
+           console.log(tempX)
+          console.log(subsequence)
 
   return subsequence;
 }
 
 
-console.log(LCS('FJKASDFNK','SADFKJLML'))
+LCS('notatest','anothertest')
+
+
 
 
 
